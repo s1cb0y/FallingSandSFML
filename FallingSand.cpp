5533dcaf24ad52e0404e7a80f809c72e1e135e6f
s1cb0y <m.spandel86@gmail.com>
2024-03-02 09:37:59 +0100

    use grid of sandcorns instead of vectors of sandcorn to improve CPU workload

--- FallingSand.cpp
+++ FallingSand.cpp
@@ -2,59 +2,19 @@
 #include <iostream>
 #include <stdlib.h>
 
-#define SAND_RAD 8.0f
-#define WINDOW_X 1024
-#define WINDOW_Y 768
-#define GRAVITY_ACC 300.0f
+#define SAND_RAD 4
+#define GRID_X 100
+#define GRID_Y 100
+#define WINDOW_X 800
+#define WINDOW_Y 800
+#define SCAL_FACTOR_X 8
+#define SCAL_FACTOR_Y 8
 
-class SandCorn{
-public:    
-    SandCorn(sf::Vector2f pos) {        
-        m_Shape.setRadius(SAND_RAD);
-        m_Shape.setPosition(pos);
-        m_Shape.setFillColor(sf::Color(rand() % 255,rand() % 255,rand() % 255));
-        m_InitialPos = pos;        
-    }
-    void Update(const sf::Time& ts, std::vector<SandCorn>& sandCorns){
-        if (!HitGround()) 
-            m_LifeTime += ts;
-            sf::Vector2f currentPos = m_Shape.getPosition();
-            // Check if new position will colide with any other sandcorn
-            m_Shape.setPosition(m_Shape.getPosition().x, m_InitialPos.y + (0.5 * GRAVITY_ACC * m_LifeTime.asSeconds() *m_LifeTime.asSeconds()));
-            // for (std::vector<SandCorn>::reverse_iterator it = sandCorns.rbegin(); it != sandCorns.rend(); it++){                
-            //     if (it->Intersects(*this)){
-            //         m_Shape.setPosition(currentPos);
-            //         m_Shape.setFillColor(sf::Color::Red);
-            //         //   std::cout<< "Break" << std::endl;
-            //         break;
-            //     } 
-
-                
-            // }
-           // std::cout << "Pos : " << m_Shape.getPosition().x << " , " <<  m_Shape.getPosition().y << std::endl;
-    }
-    
-    bool Intersects(const SandCorn& corn){
-        return m_Shape.getGlobalBounds().intersects(corn.GetShape().getGlobalBounds());
-    }
-
-    bool HitGround() {return m_Shape.getPosition().y >= WINDOW_Y - 2*SAND_RAD; }
-
-    sf::CircleShape GetShape() const { return m_Shape;}    
-
-private:
-    sf::CircleShape m_Shape; 
-    sf::Vector2f m_InitialPos;
-    sf::Time m_LifeTime;
-};
-
-class SandBox{
+class World{
 public:
-    SandBox(){        
-        m_Grid.resize(WINDOW_Y * WINDOW_X);        
-        std::fill(m_Grid.begin(), m_Grid.end(), false);        
+    World() {        
     }
-    ~SandBox(){}
+    ~World(){}
 
     void Update(sf::RenderWindow& window, const sf::Time& ts){
         
@@ -64,51 +24,79 @@ public:
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+        
         //get current mouse position
-        sf::Vector2i mPos = sf::Mouse::getPosition(window);    
+        sf::Vector2i mousePos = sf::Mouse::getPosition(window);    
+        // only create new sandcorn of mouse position is on screen
+        if (MouseOnScreen(mousePos)){                        
+            // scale screen position to grid position
+            m_Grid[mousePos.y / (SCAL_FACTOR_Y)][mousePos.x / (SCAL_FACTOR_X)] = true;
+        }        
         
-        // update postion of all sand corns
-        for (std::vector<SandCorn>::reverse_iterator it = m_SandCorns.rbegin(); it != m_SandCorns.rend(); it++){
-            it->Update(ts, m_SandCorns);
+        // Update all sandcorn
+        for (int x = 0 ; x < GRID_X-2; x++){
+            for (int y = 0 ; y < GRID_Y-2; y++){
+                if (m_GridBuffer[y][x] == true){
+                    if (m_GridBuffer[y+1][x] == false){
+                        m_Grid[y+1][x] = true;
+                        m_Grid[y][x]   = false;
+                    }
+                    else if (m_GridBuffer[y+1][x-1] == false){
+                        m_Grid[y+1][x-1] = true;
+                        m_Grid[y][x]   = false;
+                    }
+                    else if (m_GridBuffer[y+1][x+1] == false){
+                        m_Grid[y+1][x+1] = true;
+                        m_Grid[y][x]   = false;
+                    }
+                }
+            }
         }
-
-        SandCorn corn(sf::Vector2f(mPos.x - SAND_RAD *0.5f , mPos.y - SAND_RAD * 0.5f));
-        if (m_SandCorns.size() == 0)
-            m_SandCorns.push_back(corn);
-        // only create new sandcorn if it does not intersect with last one
-        if (!corn.Intersects(m_SandCorns.back())){
-            m_SandCorns.push_back(corn);
-        }
-            
-        
     }
     void DrawSand(sf::RenderWindow& window){
-        //window.draw(m_SandCorns.back());
-        for (auto& sand : m_SandCorns){            
-            window.draw(sand.GetShape());
+                
+        sf::CircleShape m_Shape; 
+        m_Shape.setRadius(SAND_RAD);
+        
+        for (int y = 0 ; y < GRID_Y; y++){
+            for (int x = 0 ; x < GRID_X; x++){
+                if (m_Grid[y][x]){
+                    m_Shape.setFillColor(sf::Color(rand() % 255,rand() % 255,rand() % 255));
+                    // calculate position on screen            
+                    m_Shape.setPosition(x * SCAL_FACTOR_X, y * SCAL_FACTOR_Y);
+                    window.draw(m_Shape);
+                }
+                m_GridBuffer[y][x] = m_Grid[y][x];
+            }
         }
     }
 
-private:     
-    std::vector<SandCorn> m_SandCorns;
-    std::vector<bool> m_Grid;
+    bool MouseOnScreen(sf::Vector2i mousePos){
+        return (mousePos.x >= 0 && mousePos.y >= 0 && mousePos.x < WINDOW_X && mousePos.y < WINDOW_Y);
+    }
+
+private:         
+    bool m_Grid[GRID_Y][GRID_X] = {false};
+    bool m_GridBuffer[GRID_Y][GRID_X] = {false};
     
 };
 
 
 int main()
-{
+{     
     sf::RenderWindow window(sf::VideoMode(WINDOW_X, WINDOW_Y), "Falling Sand!");
-    SandBox sandBox;
+    World world;
+    window.setFramerateLimit(60);
     sf::Clock clock;
     while (window.isOpen())
     {    
         sf::Time elapsed = clock.restart();    
-        sandBox.Update(window, elapsed);
+        world.Update(window, elapsed);
 
         window.clear();
-        sandBox.DrawSand(window);
+        world.DrawSand(window);
         window.display();
+     
     }
 
     return 0;
